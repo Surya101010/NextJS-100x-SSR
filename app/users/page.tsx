ac91f08895132641e8461244e8feb00f7a4e3f10
surya <greatuchsurya@gmail.com>
2025-12-10 16:45:17 +0530

    added api in nextjs

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,24 +1,28 @@
 "use client"
 import axios from "axios";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-export default function Userpage(){
-    const [loading,setLoading]=useState(true);
-    const [data,setData]=useState();
-    useEffect(()=>{
-        axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details").then(response =>{
-            //@ts-ignore
-            setData(response.data);
-            setLoading(false);
-    })
-    },[])
-    if (loading) {
-        return <div>
-            loading...
-        </div>
-    }
+export default async function Userpage(){
+    // const [loading,setLoading]=useState(true);
+    // const [data,setData]=useState();
+    // useEffect(()=>{
+    //     axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details").then(response =>{
+    //         //@ts-ignore
+    //         setData(response.data);
+    //         setLoading(false);
+    // })
+    // },[])
+    // if (loading) {
+    //     return <div>
+    //         loading...
+    //     </div>
+    // }
+    const response = await axios.get("http://localhost:3000/api/v1/user/details")
+    const data1 : any= response.data;
 
 return(<div>
-        {data.name}
+        {data1.name}
+        <br />
+        {data1.email}
     </div>)
 }
